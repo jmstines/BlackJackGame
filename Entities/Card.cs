e27a36d2cb9e48afddf729cc78c0d6cebfcd16f1
Jeffrey Stines <jmstines00@gmail.com>
2019-03-25 23:16:32 -0400

    Added error and test for Card Value > 12

--- Entities/Card.cs
+++ Entities/Card.cs
@@ -14,7 +14,15 @@ namespace Entities
       Suit = suit;
       Display = display ?? throw new ArgumentNullException(nameof(display));
       Description = description ?? throw new ArgumentNullException(nameof(description));
-      if (value < 1) throw new ArgumentOutOfRangeException(nameof(value));
+      if (value < 1)
+      {
+        throw new ArgumentOutOfRangeException(nameof(value), "Card values must be Greater than 0.");
+      }
+      else if (value > 11)
+      {
+        throw new ArgumentOutOfRangeException(nameof(value), "Card values must be Less than 12.");
+      }
+
       Value = value;
     }
   }

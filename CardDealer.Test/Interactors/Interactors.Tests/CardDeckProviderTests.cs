3d8d607b88735080a08b53ff3bf7ae2a52ca8afe
Jeffrey Stines <jmstines00@gmail.com>
2019-09-08 22:01:39 -0400

    CardDeckProvider static method to readonly, renamed tests to match class

--- CardDealer.Test/Interactors/Interactors.Tests/CardDeckProviderTests.cs
+++ CardDealer.Test/Interactors/Interactors.Tests/CardDeckProviderTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 namespace Interactors.Tests
 {
-    public class DealCardsInteractorTests
+    public class CardDeckProviderTests
     {
         [SetUp]
         public void Setup()
@@ -18,10 +18,10 @@ namespace Interactors.Tests
         public void CreateNewDeck_CardCount_52()
         {
             IEnumerable<Card> deck = new CardDeckProvider().Deck;
-            var count = deck.ToList().Count;
-            Assert.AreEqual(deck.ToList().Count, 52);
+            Assert.AreEqual(52, deck.Count());
         }
 
+        [Test]
         public void CreateNewDeck_2ofClubs_FirstCard()
         {
             IEnumerable<Card> deck = new CardDeckProvider().Deck;
@@ -30,12 +30,25 @@ namespace Interactors.Tests
             Assert.AreEqual(deck.First(), card);
         }
 
+        [Test]
         public void CreateNewDeck_AceOfSpades_LastCard()
         {
             IEnumerable<Card> deck = new CardDeckProvider().Deck;
             var card = deck.Single(c => c.Display.Equals("A") && c.Suit.Equals(Suit.Spades));
 
             Assert.AreEqual(deck.Last(), card);
+        }
+
+        [Test]
+        public void CreateCustomDeck_AceOfSpades_AllCards()
+        {
+            IEnumerable<CardDetail> CardDetails = new List<CardDetail>{
+                new CardDetail("A", "Ace"), new CardDetail("A", "Ace"),
+                new CardDetail("A", "Ace"), new CardDetail("A", "Ace")};
+
+            IEnumerable<Card> deck = new CardDeckProvider(CardDetails).Deck;
+            Assert.AreEqual(deck.Count(), 16);
+            Assert.AreEqual(deck.Where(c => c.Display.Equals("A")).Count(), 16);
         }
     }
 }
--- Interactors.cs/Providers/CardDeckProvider.cs
+++ Interactors.cs/Providers/CardDeckProvider.cs
@@ -7,28 +7,31 @@ namespace Interactors.Providers
 {
     public class CardDeckProvider : ICardDeckProvider
     {
-        private static readonly IEnumerable<Suit> Suits = new List<Suit> { Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades };
-        private static IEnumerable<CardDetail> CardDetails = new List<CardDetail>{
-        new CardDetail("2","2"), new CardDetail("3", "3"),
-        new CardDetail("4", "4"), new CardDetail("5", "5"),
-        new CardDetail("6", "6"), new CardDetail("7", "7"),
-        new CardDetail("8", "8"), new CardDetail("9", "9"),
-        new CardDetail("10", "10"), new CardDetail("J", "Jack"),
-        new CardDetail("Q", "Queen"), new CardDetail("K", "King"),
-        new CardDetail("A", "Ace")};
+        private readonly IEnumerable<Suit> Suits = new List<Suit> { Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades };
+        private readonly IEnumerable<CardDetail> CardDetails = new List<CardDetail>{
+            new CardDetail("2","2"), new CardDetail("3", "3"),
+            new CardDetail("4", "4"), new CardDetail("5", "5"),
+            new CardDetail("6", "6"), new CardDetail("7", "7"),
+            new CardDetail("8", "8"), new CardDetail("9", "9"),
+            new CardDetail("10", "10"), new CardDetail("J", "Jack"),
+            new CardDetail("Q", "Queen"), new CardDetail("K", "King"),
+            new CardDetail("A", "Ace")};
 
         public IEnumerable<Card> Deck { get; private set; }
 
-        public CardDeckProvider() => Deck = BuildDefualtDeck();
+        public CardDeckProvider()
+        {
+            Deck = BuildDefualtDeck();
+        }
 
         public CardDeckProvider(IEnumerable<CardDetail> details)
         {
             CardDetails = details ?? throw new ArgumentNullException(nameof(details));
-            BuildDefualtDeck();
+            Deck = BuildDefualtDeck();
         }
 
         public CardDeckProvider(IEnumerable<Card> deck) => Deck = new List<Card>(deck) ?? throw new ArgumentNullException(nameof(deck));
 
-        private static IEnumerable<Card> BuildDefualtDeck() => Suits.SelectMany(s => CardDetails.Select(v => new Card(s, v.Display, v.Description)));
+        private IEnumerable<Card> BuildDefualtDeck() => Suits.SelectMany(s => CardDetails.Select(d => new Card(s, d.Display, d.Description)));
     }
 }

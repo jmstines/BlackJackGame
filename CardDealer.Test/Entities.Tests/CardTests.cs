fb048d1ae5136e31452d79774a4dbf529a51a2ef
Jeffrey Stines <jmstines00@gmail.com>
2019-09-08 22:55:40 -0400

    Started adding Tests to Card class, NOT finished

--- CardDealer.Test/Entities.Tests/CardTests.cs
+++ CardDealer.Test/Entities.Tests/CardTests.cs
@@ -5,16 +5,19 @@ namespace Entities.Tests
 {
     public class CardTests
     {
+        private readonly string AceDisplay = "A";
+        private readonly string AceDescription = "Ace";
+
         [Test]
         public void NewCard_NullDisplayValue_ArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new Card(Suit.Clubs, null, "2"));
+            Assert.Throws<ArgumentNullException>(() => new Card(Suit.Clubs, null, AceDescription));
         }
 
         [Test]
         public void NewCard_NullDescription_ArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new Card(Suit.Clubs, "2", null));
+            Assert.Throws<ArgumentNullException>(() => new Card(Suit.Clubs, AceDisplay, null));
         }
     }
 }

5bafe323c81bf33d6b60d2350fe6c8de2c31da62
Jeffrey Stines <jmstines00@gmail.com>
2020-02-25 20:49:39 -0500

    Change Player Class Constructor to Expression Body Member

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -5,9 +5,6 @@ namespace Entities
 	public class Player
 	{
 		public string Name { get; private set; }
-		public Player(string name)
-		{
-			Name = name ?? throw new ArgumentNullException(nameof(name));
-		}
+		public Player(string name) => Name = name ?? throw new ArgumentNullException(nameof(name));
 	}
 }

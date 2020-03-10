0f63789cabfa67d45c45ece944b26f537cf9367b
Jeffrey Stines <jmstines00@gmail.com>
2020-03-09 21:22:29 -0400

    Removed the equals, hash & = overrides from card.cs

--- Entities/Card.cs
+++ Entities/Card.cs
@@ -14,21 +14,5 @@ namespace Entities
 			Suit = suit;
 			Rank = rank;
 		}
-
-		public override bool Equals(object obj) => obj is Card card &&
-				   Suit == card.Suit &&
-				   Rank == card.Rank;
-
-		public override int GetHashCode() => HashCode.Combine(Suit, Rank);
-
-		public static bool operator ==(Card left, Card right)
-		{
-			return left.Equals(right);
-		}
-
-		public static bool operator !=(Card left, Card right)
-		{
-			return !(left == right);
-		}
 	}
 }
